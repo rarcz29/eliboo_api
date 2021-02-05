85e475b6f7e0f13f25315e0706013e6649ab5181
rarcz29 <rafalcz29@gmail.com>
2021-02-05 20:32:55 +0100

    Set up entity framework

---
+++ Eliboo.Data/DataAccess/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using Eliboo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eliboo.Data.DataAccess
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Eliboo.Data/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Eliboo.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+    }
+}
