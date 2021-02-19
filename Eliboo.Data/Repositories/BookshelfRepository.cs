8451490d54dfb923f65659c72a9aedf89fcd0a11
rarcz29 <rafalcz29@gmail.com>
2021-02-19 10:27:49 +0100

    New repository: BookshelfRepository

---
+++ Eliboo.Data/Repositories/BookshelfRepository.cs
@@ -0,0 +1,16 @@
+﻿using Eliboo.Data.DataAccess;
+using Eliboo.Data.Entities;
+using Eliboo.Data.GenericRepository;
+
+namespace Eliboo.Data.Repositories
+{
+    class BookshelfRepository : Repository<Bookshelf>, IBookshelfRepository
+    {
+        private readonly AppDbContext _db;
+
+        public BookshelfRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ Eliboo.Data/Repositories/IBookshelfRepository.cs
@@ -0,0 +1,9 @@
+﻿using Eliboo.Data.Entities;
+using Eliboo.Data.GenericRepository;
+
+namespace Eliboo.Data.Repositories
+{
+    interface IBookshelfRepository : IRepository<Bookshelf>
+    {
+    }
+}
