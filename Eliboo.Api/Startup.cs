307e798647dba115a9a82561937351930fdb8c39
rarcz29 <rafalcz29@gmail.com>
2021-02-20 14:13:59 +0100

    Clean services registration

--- Eliboo.Api/Startup.cs
+++ Eliboo.Api/Startup.cs
@@ -1,18 +1,10 @@
+using Eliboo.Api.Installers;
 using Eliboo.Api.Options;
-using Eliboo.Api.Services;
-using Eliboo.Data.DataAccess;
-using Eliboo.Data.DataProvider;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
 
 namespace Eliboo.Api
 {
@@ -27,77 +19,8 @@ namespace Eliboo.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.InstallServicesInAssembly(Configuration);
             services.AddAutoMapper(typeof(Startup));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Eliboo.Api", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please insert JWT with Bearer into field",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        System.Array.Empty<string>()
-                    }
-                });
-            });
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.ASCII.GetBytes(
-                            Configuration.GetValue<string>("JwtSettings:Secret"))),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-            services.AddDbContext<AppDbContext>(options =>
-            {
-                options
-                    .UseNpgsql(Configuration.GetConnectionString("Develop"))
-                    .UseLoggerFactory(
-                        LoggerFactory
-                            .Create(builder => builder.AddConsole()
-                            .AddFilter((category, level)
-                                => category == DbLoggerCategory.Database.Command.Name
-                                && level == LogLevel.Information)))
-                    .EnableSensitiveDataLogging()
-                    .UseSnakeCaseNamingConvention();
-            });
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                               .AllowAnyMethod()
-                               .AllowAnyHeader();
-                    });
-            });
-            services.AddRouting(options => options.LowercaseUrls = true);
-
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddTransient<IAuthService, AuthService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
