617d48074564ce72efaf9255736dd2415131b365
rarcz29 <rafalcz29@gmail.com>
2021-02-21 17:59:38 +0100

    Add installer to the startup file

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,15 +1,9 @@
+using Eliboo.Api.Installers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Eliboo.Api
 {
@@ -25,8 +19,7 @@ namespace Eliboo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
