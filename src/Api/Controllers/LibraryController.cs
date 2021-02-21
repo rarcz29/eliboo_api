83b417a095f645ce77fa9f923d90239d7f62f2ad
rarcz29 <rafalcz29@gmail.com>
2021-02-21 21:11:33 +0100

    Add controllers

---
+++ src/Api/Controllers/LibraryController.cs
@@ -0,0 +1,43 @@
+﻿using Eliboo.Api.Contracts.Responses;
+using Eliboo.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Eliboo.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LibraryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LibraryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewLibrary()
+        {
+            // TODO: autogenerate token
+            string libToken = "asdfasdfasd";
+            var library = new Library { AccessToken = libToken };
+            _unitOfWork.Libraries.Add(library);
+            var affectedRows = await _unitOfWork.CommitAsync();
+            return affectedRows == 1
+                ? Ok(new AuthSuccessResponse { Token = libToken })
+                : StatusCode(StatusCodes.Status500InternalServerError,
+                             new FailResponse { Message = "Server cannot add new library to the database." });
+        }
+
+        //[HttpDelete]
+        //public async Task<IActionResult> RemoveLibrary([FromBody] TokenRequest request)
+        //{
+        //    // TODO
+        //    return BadRequest();
+        //}
+    }
+}
---
+++ src/Api/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Eliboo.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Eliboo.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpDelete()]
+        public async Task<IActionResult> RemoveUser()
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            _unitOfWork.Users.Remove(userId);
+            var libraryId = await _unitOfWork.Users.GetLibraryIdAsync(userId);
+            var numberOfUsers = await _unitOfWork.Libraries.GetNumberOfUsersAsync(libraryId);
+
+            if (numberOfUsers < 2)
+            {
+                _unitOfWork.Libraries.Remove(libraryId);
+            }
+
+            var affected = await _unitOfWork.CommitAsync();
+            return affected > 0 ? Ok() : StatusCode(500);
+        }
+    }
+}
