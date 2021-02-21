e48e0a969f879b5ab8a2419bee1fcacd5d8f4083
rarcz29 <rafalcz29@gmail.com>
2021-02-21 16:53:26 +0100

    Update entities

---
+++ src/Domain/Entities/Library.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Eliboo.Domain.Entities
+{
+    public class Library
+    {
+        public int Id { get; set; }
+        public string AccessToken { get; set; }
+
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<Bookshelf> Bookshelves { get; set; }
+    }
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Eliboo.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public int LibraryId { get; set; }
+        public Library Library { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
