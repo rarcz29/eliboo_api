7ace97b1cd6d6f84a689665471dbd550aa442b86
rarcz29 <rafalcz29@gmail.com>
2021-02-22 10:13:59 +0100

    Update startup

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -41,7 +41,7 @@ namespace Eliboo.Api
             app.UseRouting();
             app.UseCors();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
