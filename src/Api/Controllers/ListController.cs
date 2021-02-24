0cc5b115ec8b090cd69f29d7d9db42396825477c
rarcz29 <rafalcz29@gmail.com>
2021-02-24 15:09:48 +0100

    Create new contoller - ListController

---
+++ src/Api/Controllers/ListController.cs
@@ -0,0 +1,28 @@
+﻿using Eliboo.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Eliboo.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ListController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ListController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+        }
+    }
+}
